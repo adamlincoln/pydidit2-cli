51c6d8c6d5f319ff3e1d67f3fbc6175467e8fe15
Adam J. Lincoln <adamjlincoln@gmail.com>
2025-07-10 14:44:29 -0400

    get, put new, and delete functionality

--- pydiditcli/__init__.py
+++ pydiditcli/__init__.py
@@ -1,11 +1,33 @@
+import os
+
+import pydiditbackend as backend
 import typer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker as sqlalchemy_sessionmaker
 
+app = typer.Typer()
 
-def _main(name: str):
-    print(f"Hello {name}")
+sessionmaker = sqlalchemy_sessionmaker(create_engine(os.environ["PYDIDIT_DB_URL"]))
+backend.prepare(sessionmaker)
 
-def main():
-    typer.run(_main)
+@app.command()
+def get(model_name: str) -> None:
+    backend.prepare(sessionmaker)
+    print(backend.get(model_name))
+
+@app.command()
+def put(model_name: str, primary_descriptor: str) -> None:
+    model = getattr(backend.models, model_name)
+    kwargs = {
+        model.primary_descriptor: primary_descriptor,
+        "state": backend.models.enums.State.active,
+    }
+    instance = model(**kwargs)
+    backend.put(instance)
+
+@app.command()
+def delete(model_name: str, instance_id: int) -> None:
+    backend.delete(model_name, instance_id)
 
 if __name__ == "__main__":
-    main()
+    app()
