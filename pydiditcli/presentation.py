d40733c01476ae256072b98f74943503cfca5194
Adam J. Lincoln <adamjlincoln@gmail.com>
2025-07-29 21:11:28 -0400

    strikethrough for complete descriptions

--- pydiditcli/presentation.py
+++ pydiditcli/presentation.py
@@ -11,7 +11,7 @@ def todo_rich(todo) -> str:
         now = datetime.now()
         due_past_threshold = todo.due - now < todo_due_threshold
     due = "" if todo.due is None else f"Due: {"[bold red]" if due_past_threshold else "[blue]"}{todo.due.isoformat()}{"[/bold red]" if due_past_threshold else "[/blue]"} "
-    return f"[bold]{todo.description}[/bold] (ID {todo.id}, {todo.state}) {due}{escape("[")}Tags: {", ".join(tag.name for tag in todo.tags)}{escape("]")} {escape("[")}Projects: {", ".join(project.description for project in todo.contained_by_projects)}{escape("]")}{", ".join(f":notebook:{note.id}" for note in todo.notes)}"
+    return f"[bold]{"[strike]" if todo.state == "completed" else ""}{todo.description}{"[/strike]" if str(todo.state) == "completed" else ""}[/bold] (ID {todo.id}, {todo.state}) {due}{escape("[")}Tags: {", ".join(tag.name for tag in todo.tags)}{escape("]")} {escape("[")}Projects: {", ".join(project.description for project in todo.contained_by_projects)}{escape("]")}{", ".join(f":notebook:{note.id}" for note in todo.notes)}"
 
 def project_rich(project) -> str:
     due_past_threshold = False
@@ -19,7 +19,7 @@ def project_rich(project) -> str:
         now = datetime.now()
         due_past_threshold = project.due - now < project_due_threshold
     due = "" if project.due is None else f"Due: {"[bold red]" if due_past_threshold else "[blue]"}{project.due.isoformat()}{"[/bold red]" if due_past_threshold else "[/blue]"} "
-    return f"[bold]{project.description}[/bold] (ID {project.id}, {project.state}) {due}{", ".join(f":notebook:{note.id}" for note in project.notes)}:\n  [italic]{"\n  ".join(f"* {todo.description} (ID {todo.id}, {todo.state}){", ".join(f":notebook:{note.id}" for note in todo.notes)}" for todo in project.contain_todos)}[/italic]"
+    return f"[bold]{"[strike]" if project.state == "completed" else ""}{project.description}{"[/strike]" if project.state == "completed" else ""}[/bold] (ID {project.id}, {project.state}) {due}{", ".join(f":notebook:{note.id}" for note in project.notes)}:\n  [italic]{"\n  ".join(f"* {todo.description} (ID {todo.id}, {todo.state}){", ".join(f":notebook:{note.id}" for note in todo.notes)}" for todo in project.contain_todos)}[/italic]"
 
 def tag_rich(tag) -> str:
     return f"[bold]{tag.name}[/bold] (ID {tag.id}):\nTodos:\n  [italic]{"\n  ".join(f"* {todo.description} ({todo.state})" for todo in tag.todos)}[/italic]\nProjects:\n [italic]{"\n  ".join(f"* {project.description} ({project.state})" for project in tag.projects)}[/italic]"
